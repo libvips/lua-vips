8468c0309f6e13007fbbfaa02bbfbca4fff1360f
Kleis Auke Wolthuizen <github@kleisauke.nl>
2018-08-11 16:22:57 +0200

    Speed-up voperation
    Cache the length of (call-)arguments, avoid unnecessary loops and remove `table.insert` (replace it by using the `#` operator instead).

--- src/vips/voperation.lua
+++ src/vips/voperation.lua
@@ -99,16 +99,14 @@ local voperation_mt = {
             local args = {}
             local cb = ffi.cast(voperation.argumentmap_typeof,
                 function(_, pspec, argument_class, _, _, _)
-                    local name = ffi.string(pspec.name)
-
                     -- libvips uses "-" to separate parts of arg names, but we
                     -- need "_" for lua
-                    name = string.gsub(name, "-", "_")
+                    local name = string.gsub(ffi.string(pspec.name), "-", "_")
 
-                    table.insert(args, {
+                    args[#args + 1] = {
                         name = name,
                         flags = tonumber(argument_class.flags)
-                    })
+                    }
                 end)
             vips_lib.vips_argument_map(self, cb, nil, nil)
             cb:free()
@@ -125,18 +123,26 @@ local voperation_mt = {
             if vop == nil then
                 error("no such operation\n" .. verror.get())
             end
-            vop:new()
+            vop = vop:new()
 
             local arguments = vop:getargs()
+
+            -- cache the lengths
+            local arguments_length = #arguments
+            local call_args_length = #call_args
 
             log.msg("calling operation:", name)
             log.msg("passed:")
             log.msg_r(call_args)
 
+            -- make a thing to quickly get flags from an arg name
+            local flags_from_name = {}
+
             -- count required input args
             local n_required = 0
-            for i = 1, #arguments do
+            for i = 1, arguments_length do
                 local flags = arguments[i].flags
+                flags_from_name[arguments[i].name] = flags
 
                 if band(flags, INPUT) ~= 0 and
                         band(flags, REQUIRED) ~= 0 and
@@ -148,17 +154,17 @@ local voperation_mt = {
             -- so we should have been passed n_required, or n_required + 1 if
             -- there's a table of options at the end
             local last_arg
-            if #call_args == n_required then
+            if call_args_length == n_required then
                 last_arg = nil
-            elseif #call_args == n_required + 1 then
+            elseif call_args_length == n_required + 1 then
                 last_arg = call_args[#call_args]
                 if type(last_arg) ~= "table" then
-                    error("unable to call " .. name .. ": " .. #call_args ..
+                    error("unable to call " .. name .. ": " .. call_args_length ..
                             " arguments given, " .. n_required ..
                             ", but final argument is not a table")
                 end
             else
-                error("unable to call " .. name .. ": " .. #call_args ..
+                error("unable to call " .. name .. ": " .. call_args_length ..
                         " arguments given, but " .. n_required .. " required")
             end
 
@@ -181,7 +187,7 @@ local voperation_mt = {
             end
 
             local n = 0
-            for i = 1, #arguments do
+            for i = 1, arguments_length do
                 local flags = arguments[i].flags
 
                 if band(flags, INPUT) ~= 0 and
@@ -197,15 +203,9 @@ local voperation_mt = {
             end
 
             if last_arg then
-                local args_by_name = {}
-                for i = 1, #arguments do
-                    args_by_name[arguments[i].name] = arguments[i].flags
-                end
-
                 for k, v in pairs(last_arg) do
-                    if not vop:set(k, args_by_name[k], match_image, v) then
-                        error("unable to call " .. name .. "\n" ..
-                                verror.get())
+                    if not vop:set(k, flags_from_name[k], match_image, v) then
+                        error("unable to call " .. name .. "\n" .. verror.get())
                     end
                 end
             end
@@ -214,14 +214,14 @@ local voperation_mt = {
             if vop2 == nil then
                 error("unable to call " .. name .. "\n" .. verror.get())
             end
-            vop2:new()
-            vop = vop2
+            vop = vop2:new()
 
             local result = {}
             local vob = vop:vobject()
 
+            -- fetch required output args, plus modified input images
             n = 1
-            for i = 1, #arguments do
+            for i = 1, arguments_length do
                 local flags = arguments[i].flags
 
                 if band(flags, OUTPUT) ~= 0 and
@@ -240,7 +240,8 @@ local voperation_mt = {
                 end
             end
 
-            for i = 1, #arguments do
+            --  fetch optional output args
+            for i = 1, arguments_length do
                 local flags = arguments[i].flags
 
                 if band(flags, OUTPUT) ~= 0 and
