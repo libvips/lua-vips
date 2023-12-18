65359c087e7b50af47adc11c3a8dbbb03bedcad2
John Cupitt <jcupitt@gmail.com>
2023-12-18 11:25:26 +0000

    try adding interpolate

---
+++ src/vips/Interpolate.lua
@@ -0,0 +1,34 @@
+-- make image interpolators, see affine
+
+local ffi = require "ffi"
+
+local verror = require "vips.verror"
+local vobject = require "vips.vobject"
+
+local vips_lib = ffi.load(ffi.os == "Windows" and "libvips-42.dll" or "vips")
+
+local Interpolate = {}
+
+Interpolate.vobject = function(self)
+    return ffi.cast(vobject.typeof, self)
+end
+
+Interpolate.new = function(self)
+    return vobject.new(self)
+end
+
+Interpolate.new_from_name = function(name)
+    -- there could potentially be other params here, but ignore that for now
+    local interpolate = vips_lib.vips_interpolate_new(name)
+    if interpolate == nil then
+        error("no such interpolator\n" .. verror.get())
+    end
+
+    return Interpolate.new(interpolate)
+end
+
+return ffi.metatype("Interpolate", {
+    __index = Interpolate
+})
+
+
